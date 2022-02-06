0dab7b12055f98d0b142b52986a2905dd9b83fce
lujohnny95 <lu_johnny95@yahoo.com>
2022-02-06 21:39:26 +0000

    PlaceDetails cards added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const [places, setPlaces] = useState([]);
 
     const [coordinates, setCoordinates] = useState({ });
-    const [bounds, setBounds] = useState(null);
+    const [bounds, setBounds] = useState({ });
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(( { coords: { latitude, longitude } }) => {
@@ -19,7 +19,7 @@ const App = () => {
     }, [])
 
     useEffect(() => {
-        getPlacesData();
+        getPlacesData(bounds.sw, bounds.ne);
             await ((data) => {
                 console.log(data);
 
